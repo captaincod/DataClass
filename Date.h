3fb435f8df490d34daf3bc056b37c47f9b05e2fd
oleska <46486971+captaincod@users.noreply.github.com>
2021-01-22 12:07:46 +0800

    Добавлено время

--- Date.h
+++ Date.h
@@ -12,14 +12,17 @@ class Date {
 	int* day;
 	int* month;
 	int* year;
+	int* hours;
+	int* minutes;
 public:
 
-	Date(int _day, int _month, int _year = current_year);
+	Date(int _day, int _month, int _year = current_year, int _hours = 0, int _minutes = 0);
 
 	~Date();
 
 	bool isCorrect(); // Проверяет корректность даты
 	bool isCorrect(int d, int m);
+	bool isCorrectTime(int hour, int minutes);
 
 	string whatDay(); // Вычисляет день недели для заданной даты
 	string whatDay(int day, int month);
@@ -27,17 +30,24 @@ public:
 	int interval(int second_day, int second_month, int second_year); // Количество дней между датами
 	int interval(int second_day, int second_month);
 
+	string timeInterval(int second_day, int second_month, int second_year = current_year, int second_hours = 0, int second_minutes = 0);
+
 	void longDate(); // Выводит в формате 1.01.2001
 
 	void shortDate(); // 01.01.01
 
-	void stringDate();  // 1 января 2001
+	void stringDate(); // 1 января 2001
 
-	bool isLeap(int year); // // Проверяет на високосность
+	bool isLeap(int year); // Проверяет на високосность
 
 	int JDN(int day, int month, int year); // Вычисление номера юлианского дня по дате григорианского календаря
+
+	
 };
 
 class DateException {};
+
+
+
 
 
